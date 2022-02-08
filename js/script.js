526d0a76ad91751f39abcb110f524e073262e5d2
alena <alen.sofronova@gmail.com>
2022-02-08 17:03:29 +0300

    task1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-alert(1)
+
+// Учитывая массив целых чисел, ваше решение должно найти наименьшее целое число.
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+     return Math.min(...args);
+    }
+  }
+
